f2d1014259de0bdfa5015d08cffd3eb240950467
zumi0102 <tanakafuka0102@gmail.com>
2022-12-26 18:47:27 +0900

    [Dynamic Routes] dateのエラー時に空文字を返す

--- components/date.js
+++ components/date.js
@@ -2,5 +2,8 @@ import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
+  if (Number.isNaN(date.getTime())) {
+    return <></>;
+  }
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
